483e5c3e9a2a963e2b067b94d75df772e383cd22
Rasmus <60469087+rgehrkedk@users.noreply.github.com>
2024-12-07 20:58:09 +0100

    Create Icon.js

---
+++ components/atoms/Icon/Icon.js
@@ -0,0 +1,19 @@
+const Icon = ({ children, isDarkMode, currentTheme, size = 'md' }) => {
+  const sizes = {
+    sm: 'w-4 h-4',
+    md: 'w-5 h-5',
+    lg: 'w-6 h-6'
+  };
+
+  return (
+    <div className={`rounded-xl flex items-center justify-center
+      ${isDarkMode 
+        ? `${themes[currentTheme].subtle} text-white` 
+        : `${themes[currentTheme].subtle} text-gray-900`}
+      ${sizes[size]}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Icon;
