a858e2eb705a27c745b82bf90e6812abf25a3e03
Rasmus <60469087+rgehrkedk@users.noreply.github.com>
2024-12-08 23:57:10 +0100

    Update Switch.js

--- components/atoms/Switch/Switch.js
+++ components/atoms/Switch/Switch.js
@@ -4,7 +4,7 @@ const Switch = ({ isActive, onChange, currentTheme, isDarkMode }) => {
       onClick={onChange}
       className={`w-11 h-6 rounded-full relative transition-all duration-200 
         ${isActive 
-          ? themes[currentTheme].primary
+          ? mythemes[currentTheme].primary
           : isDarkMode ? 'bg-gray-600' : 'bg-gray-300'}`}
     >
       <div className={`absolute top-1 w-4 h-4 rounded-full bg-white shadow-sm
