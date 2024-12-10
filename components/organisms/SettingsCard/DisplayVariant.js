6e23760c638ceb7fddf0c46dbe30f64b0e7d9980
Rasmus <60469087+rgehrkedk@users.noreply.github.com>
2024-12-10 15:55:23 +0100

    Update DisplayVariant.js

--- components/organisms/SettingsCard/DisplayVariant.js
+++ components/organisms/SettingsCard/DisplayVariant.js
@@ -21,7 +21,7 @@ const DisplayVariant = ({
         isDarkMode={isDarkMode}
         onToggle={onToggle}
         currentTheme={currentTheme}
-        themes={mythemes}
+        themes={themes}
       />
     </SettingsCard>
   );
